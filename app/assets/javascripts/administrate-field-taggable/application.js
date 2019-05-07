cb55d7c9360261796fe7f96bac756e2c55b33b9f
Wyatt Kirby <wyatt@apsis.io>
2019-05-07 12:51:22 -0700

    Fix jquery selector

--- app/assets/javascripts/administrate-field-taggable/application.js
+++ app/assets/javascripts/administrate-field-taggable/application.js
@@ -1,6 +1,6 @@
 // ActsAsTaggableOn Administrate Form
 $(function() {
-  $(".field-unit--taggable-field .field-unit__field > input").each(function(elem) {
+  $(".field-unit--acts-as-taggable .field-unit__field > input").each(function(elem) {
     var $this = $(this);
     var opts = $this.data('tag-options');
 
